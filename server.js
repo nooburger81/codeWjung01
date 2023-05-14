e972b009ca83cc87cb124070233a7c38b675762e
nooburger81 <brandonsebastian@gmail.com>
2023-05-14 10:56:26 -0400

    made get request work

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
-
+const routes = require('./routes');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(routes);
+
 
 app.listen(PORT, () => {
     console.log(`STOP CRYING`);
