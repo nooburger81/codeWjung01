3edd1a0e316cb0abcd325c3888faab17580015b3
nooburger81 <brandonsebastian@gmail.com>
2023-05-16 16:27:22 -0400

    made a dashboard and showed specific data

--- controllers/pages/index.js
+++ controllers/pages/index.js
@@ -1,7 +1,33 @@
 const router = require('express').Router();
+const { User } = require("../../models");
+// localhost:3001
+router.get('/', async (req, res) => {
+    const userData = await User.findAll({
+        attributes: [
+            'id',
+            'username'
+        ]
+    });
+    console.log("UGLY DATA BOO", userData);
 
-router.get('/', (req, res) => {
-    res.render('homepage');
+    const prettyUserData = userData.map((user) => user.get({plain: true}));
+    console.log("PRETTY USER DATA YAY", prettyUserData);
+    res.render('homepage', {prettyUserData});
 })
+
+// localhost:3001/dashboard
+router.get('/dashboard/:user_id', async (req, res) => {
+    const userData = await User.findByPk(req.params.user_id, {
+        attributes: [
+            'username',
+            'email'
+        ]
+    });
+    console.log("This is the ugly sequelize object!", userData);
+
+    const actualUserData = userData.get({ plain: true });
+    console.log("This is SOOO handsome!", actualUserData);
+    res.render('dashboard', actualUserData);
+});
 
 module.exports = router;
